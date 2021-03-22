2a14e90ecca5c27d5267c36ce5da5e8dca9ee424
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2021-03-22 00:16:45 -0300

    remove: remove alert message

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,7 +17,7 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (!newTaskTitle) {
-      alert("A tarefa deve possuir um título");
+      return;
     }
 
     const newTask = {
